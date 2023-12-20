20cc99b8fb6cad78fc970e05d441c78fdec74e2f
Rafał Augustyniak <Augustyniak@users.noreply.github.com>
2023-12-20 15:30:33 -0500

    add Package.swift file (#6)

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "Capture",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "Capture",
+            targets: ["Capture"]
+        )
+    ],
+    targets: [
+       .binaryTarget(
+           name: "Capture",
+           url: "https://dl.bitdrift.io/sdk/ios/capture-0.9.100/Capture.zip",
+           checksum: "93551414532badc8b609ed5632c59b9b0acf742ebb332deba7904a54ebf62113"
+       ),
+    ]
+)
