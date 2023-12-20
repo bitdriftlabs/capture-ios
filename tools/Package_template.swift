10b1ea357973c5ccc221c1ff3da238b98ccfaa70
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2023-12-20 11:54:48 -0500

    implement swiftpm release job

---
+++ tools/Package_template.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "Capture",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "Capture",
+            targets: ["Capture"]
+        )
+    ],
+    targets: [
+       .binaryTarget(
+           name: "Capture",
+           url: "https://dl.bitdrift.io/sdk/ios/capture-{VERSION}/Capture.zip",
+           checksum: "{CHECKSUM}"
+       ),
+    ]
+)
