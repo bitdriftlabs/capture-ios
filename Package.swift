5e17ee3727b38e9591e4dcf7f1e0dee0efe5cf99
Augustyniak <Augustyniak@users.noreply.github.com>
2023-12-21 16:35:36 +0000

    Release new version: 0.9.101

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
        .binaryTarget(
            name: "Capture",
-           url: "https://dl.bitdrift.io/sdk/ios/capture-0.9.100/Capture.zip",
-           checksum: "93551414532badc8b609ed5632c59b9b0acf742ebb332deba7904a54ebf62113"
+           url: "https://dl.bitdrift.io/sdk/ios/capture-0.9.101/Capture.zip",
+           checksum: "3ca16080ec38254e0a0bd356746e4a9dfaecf7dc6db7f8f835eed5a2e336d681"
        ),
         .testTarget(
             name: "CaptureUnit",
