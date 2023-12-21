42a0ce393b7d26e9c98f52cbe3de9e4b7adcc405
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2023-12-20 21:59:55 -0500

    fix

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
        .binaryTarget(
            name: "Capture",
-           url: "https://dl.bitdrift.io/sdk/ios/capture-0.9.100/Capture.zip",
-           checksum: "93551414532badc8b609ed5632c59b9b0acf742ebb332deba7904a54ebf62113"
+           url: "https://dl.bitdrift.io/sdk/ios/capture-456/Capture.zip",
+           checksum: "123"
        ),
         .testTarget(
             name: "CaptureUnit",
