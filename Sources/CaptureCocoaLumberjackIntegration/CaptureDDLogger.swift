15281761c9192ad33a012b4f2762751447c547d1
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-03-29 14:56:12 -0400

    add logger integrations

---
+++ Sources/CaptureCocoaLumberjackIntegration/CaptureDDLogger.swift
@@ -0,0 +1,59 @@
+import Capture
+import CocoaLumberjack
+import os
+
+/// The wrapper around Capture SDK logger that conforms to `DDLogger` protocol from `CocoaLumberjack`
+/// library and can be used as a drop-in solution for forwarding `CocoaLumberjack` logs to bitdrift
+/// Capture SDK.
+final class CaptureDDLogger: NSObject, DDLogger {
+    private let osLogger = OSLogger()
+
+    var logFormatter: DDLogFormatter? = nil
+
+    func log(message logMessage: DDLogMessage) {
+        guard let level = LogLevel(logMessage.level) else {
+            return
+        }
+
+        Capture.Logger.log(
+            level: level,
+            message: logMessage.message
+        )
+    }
+
+    func didAdd() {
+        let isCaptureLoggerConfigured = Capture.Logger.sessionID != nil
+        if !isCaptureLoggerConfigured {
+            self.osLogger.log(
+                level: .error,
+                message: "`CaptureDDLogger` added as output target of `DDLogger` before the configuration of Capture SDK. Logs emitted prior to the configuration of the Capture SDK will not be visible to the SDK."
+            )
+        }
+    }
+}
+
+extension LogLevel {
+    /// Initializes a new instance of Capture log level using provided Cocoa Lumberjack log level.
+    ///
+    /// - parameter logLevel: Cocoa Lumberjack log level.
+    public init?(_ logLevel: DDLogLevel) {
+        switch logLevel {
+        case .off:
+            return nil
+        case .error:
+            self = .error
+        case .warning:
+            self = .warning
+        case .info:
+            self = .info
+        case .debug:
+            self = .debug
+        case .verbose:
+            self = .trace
+        case .all:
+            self = .trace
+        default:
+            self = .trace
+        }
+    }
+}
---
+++ Sources/CaptureSwiftyBeaverIntegration/CaptureSwiftyBeaverLogger.swift
@@ -0,0 +1,53 @@
+import Capture
+import SwiftyBeaver
+
+/// The wrapper around Capture SDK logger that conforms to `BaseDestination` protocol from `SwiftyBeaver`
+/// library and can be used as a drop-in solution for forwarding `SwiftyBeaver` logs to bitdrift
+/// Capture SDK.
+final class CaptureSwiftyBeaverLogger: BaseDestination {
+    public override func send(
+        _ level: SwiftyBeaver.Level,
+        msg: String,
+        thread: String,
+        file: String,
+        function: String,
+        line: Int,
+        context: Any? = nil
+    ) -> String?
+    {
+        Capture.Logger.log(
+            level: LogLevel(level),
+            message: msg
+        )
+
+        return super.send(
+            level,
+            msg: msg,
+            thread: thread,
+            file: file,
+            function: function,
+            line: line,
+            context: context
+        )
+    }
+}
+
+extension Capture.LogLevel {
+    /// Initializes a new instance of Capture log level using provided Swifty Beaver log level.
+    ///
+    /// - parameter logLevel: Swifty Beaver log level.
+    init(_ logLevel: SwiftyBeaver.Level) {
+        switch logLevel {
+        case .verbose:
+            self = .trace
+        case .debug:
+            self = .debug
+        case .info:
+            self = .info
+        case .warning:
+            self = .warning
+        case .error:
+            self = .error
+        }
+    }
+}
