30c0204599513af6c74ea0ae690b16d805e89955
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-01 10:22:16 -0400

    download swift

--- ci/linux_format_setup.sh
+++ ci/linux_format_setup.sh
@@ -9,3 +9,12 @@ curl -OL https://github.com/realm/SwiftLint/releases/download/0.54.0/swiftlint_l
 unzip swiftlint_linux.zip
 mv swiftlint bin/
 chmod +x bin/swiftlint
+
+# The binaries above dynamically link a library provided by Swift, so download Swift + update the
+# LD_LIBRARY_PATH tell the system how to find them.
+swift_archive_name="swift-5.9.2-RELEASE-ubuntu22.04"
+curl -OL "https://download.swift.org/swift-5.9.2-release/ubuntu2204/swift-5.9.2-RELEASE/$swift_archive_name.tar.gz"
+tar xf "$swift_archive_name.tar.gz"
+
+echo "PATH=$(pwd):$(pwd)/$swift_archive_name/usr/bin:$PATH" >> "$GITHUB_ENV"
+echo "LD_LIBRARY_PATH=$(pwd)/$swift_archive_name/usr/lib" >> "@GITHUB_ENV"
