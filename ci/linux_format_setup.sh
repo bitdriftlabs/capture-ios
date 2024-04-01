9c1aa8116a643fbb4712e9545bd747a000952976
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-01 11:03:13 -0400

    extract to specific directory

--- ci/linux_format_setup.sh
+++ ci/linux_format_setup.sh
@@ -18,7 +18,7 @@ chmod +x bin/swiftformat
 
 # DrString - Validate documentation comments.
 curl -OL https://github.com/dduan/DrString/releases/download/0.6.1/drstring-x86_64-unknown-ubuntu.tar.gz
-tar xf drstring-x86_64-unknown-ubuntu.tar.gz
+tar xf drstring-x86_64-unknown-ubuntu.tar.gz -C drstring-x86_64-unknown-ubuntu
 ls -al
 echo "NEXT"
 ls -al usr
