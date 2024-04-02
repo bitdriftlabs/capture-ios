e107d1d810398651e4535eb64dd4a267e5fba865
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-02 13:05:59 -0400

    new release CI jobs

--- ci/cocoapods_update.sh
+++ ci/cocoapods_update.sh
@@ -6,9 +6,7 @@ readonly version="$1"
 
 # Look for a string in 'BitdriftCapture.podspec' that looks like `capture-$VERSION/Capture.zip` and substitute
 # existig version for the new one.
-sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" BitdriftCapture.podspec \
-  | sed -e "s#\(checksum: \"\)\(.*\)\(\"\)#\1$checksum\3#g" \
-  > BitdriftCapture_tmp.podspec
+sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" BitdriftCapture.podspec > BitdriftCapture_tmp.podspec
 mv BitdriftCapture_tmp.podspec BitdriftCapture.podspec
 
 echo "Generate BitdriftCapture.podspec:"
--- ci/swift_pm_package_update.sh
+++ ci/swift_pm_package_update.sh
@@ -3,9 +3,10 @@
 set -euxo pipefail
 
 readonly version="$1"
-readonly checksum="$(curl "https://dl.bitdrift.io/sdk/ios/capture-$VERSION/Capture.zip.sha256")"
+checksum="$(curl "https://dl.bitdrift.io/sdk/ios/capture-$version/Capture.zip.sha256")"
+readonly checksum=$checksum
 
-# Look for a string in 'Package.swift' that looks like `capture-$VERSION/Capture.zip` and substitute
+# Look for a string in 'Package.swift' that looks like `capture-[VERSION]/Capture.zip` and substitute
 # existig version for the new one.
 sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" Package.swift \
   | sed -e "s#\(checksum: \"\)\(.*\)\(\"\)#\1$checksum\3#g" \
