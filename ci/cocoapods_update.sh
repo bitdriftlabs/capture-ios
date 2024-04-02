a984c876c93d093c082b4108503dde16038822a2
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-02 17:04:05 -0400

    cocoapods: replace VERSION placeholder with actual version

--- ci/cocoapods_update.sh
+++ ci/cocoapods_update.sh
@@ -4,9 +4,12 @@ set -euxo pipefail
 
 readonly version="$1"
 
-# Look for a string in 'BitdriftCapture.podspec' that looks like `capture-$VERSION/Capture.zip` and substitute
+# Look for a string in 'BitdriftCapture.podspec' that looks like `capture-<VERSION>/Capture.zip` and substitute
 # existig version for the new one.
-sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" BitdriftCapture.podspec > BitdriftCapture_tmp.podspec
+sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" BitdriftCapture.podspec \
+  # Look for a "version = '<VERSION>'" string and substitute existig version for the new one.
+  | sed -e "s#\(version = \'\)\(.*\)\(\'\)#\1$version\3#g" \
+  > BitdriftCapture_tmp.podspec
 mv BitdriftCapture_tmp.podspec BitdriftCapture.podspec
 
 echo "Generate BitdriftCapture.podspec:"
