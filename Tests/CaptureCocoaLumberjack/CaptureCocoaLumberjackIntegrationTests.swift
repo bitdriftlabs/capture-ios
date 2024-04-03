3032437dbb425267410018279ef1f115427ea2e5
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-03 18:54:36 -0400

    expand tests, more logs

--- Tests/CaptureCocoaLumberjack/CaptureCocoaLumberjackIntegrationTests.swift
+++ Tests/CaptureCocoaLumberjack/CaptureCocoaLumberjackIntegrationTests.swift
@@ -5,5 +5,11 @@ import XCTest
 final class CaptureCocoaLumberjackIntegrationTests: XCTestCase {
     func testAddingCaptureDDLogger() {
         DDLog.add(CaptureDDLogger())
+
+        DDLogVerbose("Verbose")
+        DDLogDebug("Debug")
+        DDLogInfo("Info")
+        DDLogWarn("Warn")
+        DDLogError("Error")
     }
 }
--- Tests/CaptureSwiftyBeaver/CaptureSwiftyBeaverIntegrationTests.swift
+++ Tests/CaptureSwiftyBeaver/CaptureSwiftyBeaverIntegrationTests.swift
@@ -5,5 +5,11 @@ import XCTest
 final class CaptureSwiftyBeaverIntegrationTests: XCTestCase {
     func testAddingCaptureSwiftyBeaverLogger() {
         SwiftyBeaver.addDestination(CaptureSwiftyBeaverLogger())
+
+        SwiftyBeaver.verbose("Verbose")
+        SwiftyBeaver.debug("Debug")
+        SwiftyBeaver.info("Info")
+        SwiftyBeaver.warning("Warning")
+        SwiftyBeaver.error("Error")
     }
 }
