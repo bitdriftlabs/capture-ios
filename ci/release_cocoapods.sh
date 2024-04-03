497af00728b16c7ca3db18c0016c9c677487fd9c
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-03 15:21:23 -0400

    publish all podspecs to cocoapods

--- ci/release_cocoapods.sh
+++ ci/release_cocoapods.sh
@@ -2,5 +2,21 @@
 
 set -euxo pipefail
 
-echo "++ Pushing Bitf"
-pod trunk push BitdriftCapture.podspec --verbose
+echo "+ Pushing podspecs to CocoaPods"
+
+function publish_podspec() {
+  local -r pod_name="$1"
+
+    echo "++ Pushing '$pod_name' podspec to CocoaPods"
+    pod trunk push "$pod_name.podspec" --verbose
+}
+
+podspecs=(\
+  "BitdriftCapture" \
+  "CaptureCocoaLumberjack" \
+  "CaptureSwiftyBeaver" \
+)
+
+for podspec in "${podspecs[@]}"; do
+  publish_podspec "$podspec"
+done
