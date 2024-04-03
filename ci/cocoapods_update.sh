ba57d487f91211f9c0f5a44bd7d7722ee901635e
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-03 15:51:05 -0400

    update release_cocoapods script

--- ci/cocoapods_update.sh
+++ ci/cocoapods_update.sh
@@ -4,13 +4,31 @@ set -euxo pipefail
 
 readonly version="$1"
 
-# Process 'BitdriftCapture.podspec' file:
-#  * Look for a `capture-<VERSION>/Capture.zip` string and substitute existig version for the new one.
-#  * Look for a `version = '<VERSION>'` string and substitute existig version for the new one.
-sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" BitdriftCapture.podspec \
-  | sed -e "s#\(version = \'\)\(.*\)\(\'\)#\1$version\3#g" \
-  > BitdriftCapture_tmp.podspec
-mv BitdriftCapture_tmp.podspec BitdriftCapture.podspec
+echo "+ Generating podpsecs for $version version"
 
-echo "Generate BitdriftCapture.podspec:"
-cat BitdriftCapture.podspec
+function update_podspec() {
+  local -r pod_name="$1"
+  local -r podspec="$pod_name".podspec
+  local -r tmp_podspec=tmp_"$podspec"
+
+  # Process '<name>.podspec' file:
+  #  * Look for a `capture-<VERSION>/Capture.zip` string and substitute existig version for the new one.
+  #  * Look for a `version = '<VERSION>'` string and substitute existig version for the new one.
+  sed -e "s#\(capture-\)\(.*\)\(/Capture\.zip\)#\1$version\3#g" "$podspec" \
+    | sed -e "s#\(version = \'\)\(.*\)\(\'\)#\1$version\3#g" \
+    > "$tmp_podspec"
+  mv "$tmp_podspec" "$podspec"
+
+  echo "++ Generated $podspec:"
+  cat "$podspec"
+}
+
+podspecs=(\
+  "BitdriftCapture" \
+  "CaptureCocoaLumberjack" \
+  "CaptureSwiftyBeaver" \
+)
+
+for podspec in "${podspecs[@]}"; do
+  update_podspec "$podspec"
+done
--- ci/release_cocoapods.sh
+++ ci/release_cocoapods.sh
@@ -7,8 +7,8 @@ echo "+ Pushing podspecs to CocoaPods"
 function publish_podspec() {
   local -r pod_name="$1"
 
-    echo "++ Pushing '$pod_name' podspec to CocoaPods"
-    pod trunk push "$pod_name.podspec" --verbose
+  echo "++ Pushing '$pod_name' podspec to CocoaPods"
+  pod trunk push "$pod_name.podspec" --verbose
 }
 
 podspecs=(\
