527664011425a165a8270b69065f1fca3f544828
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-04 15:47:05 -0400

    better comment

--- tools/ci/release_cocoapods.sh
+++ tools/ci/release_cocoapods.sh
@@ -19,7 +19,7 @@ podspecs=(\
 
 for podspec in "${podspecs[@]}"; do
   # Make sure that the CocoaPods specs repo is up-to-date.
-  # Important since we push specs in order and one podspec might depend on another.
+  # Important, as we push specs sequentially, and one podspec might depend on another.
   rm -rf ~/.cocoapods/repos/trunk
   pod repo update --verbose
 
