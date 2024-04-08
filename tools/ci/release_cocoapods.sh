166ef8c3e1bf5c33ca6d60e8092322b04011ebca
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-04-08 19:22:25 -0400

    Fix podspec refresh

--- tools/ci/release_cocoapods.sh
+++ tools/ci/release_cocoapods.sh
@@ -20,7 +20,7 @@ podspecs=(\
 for podspec in "${podspecs[@]}"; do
   # Make sure that the CocoaPods specs repo is up-to-date.
   # Important, as we push specs sequentially, and one podspec might depend on another.
-  rm -rf ~/.cocoapods/repos/trunk
+  pod cache clean --all 
   pod repo update --verbose
 
   publish_podspec "$podspec"
