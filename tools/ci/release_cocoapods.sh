83d1c85e0d9153c85c53a2fb5d219ca029bc4468
Martín Conte Mac Donell <Reflejo@gmail.com>
2025-02-27 06:28:17 -0800

    Wait til updated pod is available (#136)

--- tools/ci/release_cocoapods.sh
+++ tools/ci/release_cocoapods.sh
@@ -13,15 +13,29 @@ function publish_podspec() {
 
 publish_podspec "BitdriftCapture"
 
-pod cache clean --all
-
 # Sleep for 1 minute to allow the CocoaPods specs repository to update. The CocoaPods repository is
 # behind a DNS system that uses caching, which prevents us from seeing the BitdriftCapture update immediately
 # after it happens."
-sleep 60
+VERSION=$(cat BitdriftCapture.podspec | grep "s.version = '" | sed "s/.*s.version = '\(.*\)'/\1/")
+
+# Execute the curl command to see if the latest pod is available in https://cdn.cocoapods.org/all_pods_versions_0_7_9.txt
+# in a loop until the version is there. The loop exits if the pod is there but also if 30 minutes has passed.
+START_TIME=$(date +%s)
+until { curl -s "https://cdn.cocoapods.org/all_pods_versions_0_7_9.txt" | gunzip | grep -q "BitdriftCapture.*/$VERSION/"; }; do
+  SECONDS=$(($(date +%s) - $START_TIME))
+  if [ $SECONDS -gt 1800 ]; then
+    echo "Timed out waiting for BitdriftCapture $VERSION to be available in the CocoaPods specs repository."
+    exit 1
+  fi
+
+  echo "BitdriftCapture $VERSION not available in the CocoaPods specs repository yet. Sleeping for 1 minute."
+  sleep 60
+done
+
 
 # Make sure that the CocoaPods specs repo is up-to-date.
 # Important, as we push specs sequentially, and one podspec might depend on another.
+pod cache clean --all
 pod repo update --verbose
 
 publish_podspec "CaptureSwiftyBeaver"
