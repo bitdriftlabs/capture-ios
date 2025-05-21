6f2ff9b081f97b1ead626754a670f1f53e8ffaa2
Martin Conte Mac Donell <reflejo@gmail.com>
2025-05-21 09:03:45 -0700

    Switch to newer Xcode

--- tools/ci/mac_ci_setup.sh
+++ tools/ci/mac_ci_setup.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 # https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md#xcode
-sudo xcode-select --switch /Applications/Xcode_15.4.app
+sudo xcode-select --switch /Applications/Xcode_16.2.app
