403c36c7825f9e322776dd975c80e57b67dfa1ea
Fran Aguilera <fran@bitdrift.io>
2025-05-21 18:22:19 +0200

    Update simulator name

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 xcodebuild \
   -scheme Capture-Package \
   -sdk iphonesimulator18.2 \
-  -destination 'platform=iOS Simulator,OS=latest,name=iPhone 15' \
+  -destination 'platform=iOS Simulator,OS=latest,name=iPhone 16' \
   test
