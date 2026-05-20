edb9bffc01aee979c60c44b8c69091ba98335b36
ArielDemarco <demarcoariel.dev@gmail.com>
2026-05-20 13:13:42 -0300

    Reverted: going to macos-15 as bc it has xcode 26.0.1 and installed runtimes and sims

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 xcodebuild \
   -scheme Capture-Package \
-  -sdk iphonesimulator \
+  -sdk iphonesimulator26.0 \
   -destination 'platform=iOS Simulator,OS=latest,name=iPhone 17' \
   test
