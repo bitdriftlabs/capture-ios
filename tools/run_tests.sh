cd212d3e166f5862990434351f5dfdf545f82ce5
ArielDemarco <demarcoariel.dev@gmail.com>
2026-05-20 13:06:28 -0300

    Removed debug + trying to fix tests job

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 xcodebuild \
   -scheme Capture-Package \
-  -sdk iphonesimulator26.0 \
+  -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,OS=latest,name=iPhone 17' \
   test
