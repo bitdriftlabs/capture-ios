09bd11f9e361e6a78d4da06aaadda82c9e24f0dc
ArielDemarco <demarcoariel.dev@gmail.com>
2026-05-20 12:38:04 -0300

    Changed to use macos26 and xcode 26.0.1

--- tools/ci/mac_ci_setup.sh
+++ tools/ci/mac_ci_setup.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-# https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md#xcode
-sudo xcode-select --switch /Applications/Xcode_16.2.app
+# https://github.com/actions/runner-images/blob/main/images/macos/macos-26-Readme.md#xcode
+sudo xcode-select --switch /Applications/Xcode_26.0.1.app
--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 xcodebuild \
   -scheme Capture-Package \
-  -sdk iphonesimulator18.2 \
-  -destination 'platform=iOS Simulator,OS=latest,name=iPhone 16' \
+  -sdk iphonesimulator26.0 \
+  -destination 'platform=iOS Simulator,OS=latest,name=iPhone 17' \
   test
